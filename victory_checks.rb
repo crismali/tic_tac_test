decf66909f44b22339bd6279cde9e4f31832ba12
Michael Crismali <michael.crismali@gmail.com>
2013-02-27 11:24:44 -0600

    defined draw? and anybody_win? methods and tests

--- victory_checks.rb
+++ victory_checks.rb
@@ -1,6 +1,17 @@
 module VictoryChecks
 
+  def draw?
+    @board.uniq.size == 2
+  end
 
+  def anybody_win?
+    lines = get_lines
+    someone_won = false
+    lines.each do |line|
+      someone_won = line.first if line.uniq.size == 1
+    end
+    return someone_won
+  end
 
 
 end
--- victory_checks_test.rb
+++ victory_checks_test.rb
@@ -5,5 +5,26 @@ require './game.rb'
 
 class GameTest < Test::Unit::TestCase
 
+  def setup
+    @game = Game.new
+  end
+
+  def test_draw_method_returns_false_if_its_not_a_draw
+    assert_equal false, @game.draw?
+  end
+
+  def test_draw_method_returns_true_if_it_is_a_draw
+    @game.board = [ 'O','O','O','O','X','X','X','X','X']
+    assert_equal true, @game.draw?
+  end
+
+  def test_anybody_win_returns_false_if_no_one_has_won
+    assert_equal false, @game.anybody_win?
+  end
+
+  def test_anybody_win_returns_who_won_if_someone_has_won
+    @game.board = ['X','X','X',4,5,6,7,8,9]
+    assert_equal 'X', @game.anybody_win?
+  end
 
 end
