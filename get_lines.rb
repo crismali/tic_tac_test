d19dec626d6b4beba03f7e9bf68609ce8bf3ccf5
Michael Crismali <michael.crismali@gmail.com>
2013-02-27 10:19:58 -0600

    get_rows returns 3 objects

--- get_lines.rb
+++ get_lines.rb
@@ -15,6 +15,12 @@ module GetLines
 
   def get_rows(board)
 
+    row_1 = board
+    row_2 = board
+    row_3 = board
+
+    return row_1, row_2, row_3
+
   end
 
   def get_columns(board)
--- get_lines_test.rb
+++ get_lines_test.rb
@@ -16,11 +16,16 @@ class GameTest < Test::Unit::TestCase
   end
 
   def test_get_diagonals_returns_2_objects
-
     diagonal_1, diagonal_2 = @game.get_diagonals(@game.board)
-
     assert_not_nil diagonal_1
     assert_not_nil diagonal_2
+  end
+
+  def test_get_rows_returns_3_objects
+    row_1, row_2, row_3 = @game.get_rows(@game.board)
+    assert_not_nil row_1
+    assert_not_nil row_2
+    assert_not_nil row_3
 
   end
 
