4704fbd944cb808b093008b5035dc3636e3c3383
Michael Crismali <michael.crismali@gmail.com>
2013-02-28 09:30:02 -0600

    wrote cfw or block method and tests

--- computer_player.rb
+++ computer_player.rb
@@ -1,19 +1,45 @@
 class ComputerPlayer
 
-  attr_accessor :which_player
+  attr_accessor :which_player, :other_player
 
   def initialize(which_player)
     @which_player = which_player
-  end
-
-  def mark_the_board(board)
-
-
-    return board
-  end
-
-  def complete_for_win_or_block(board)
+    if @which_player == 'X'
+      @other_player = 'O'
+    else
+      @other_player = 'X'
+    end
 
   end
+
+  def mark_the_board(game)
+
+
+    return game
+  end
+
+  def complete_for_win_or_block(game)
+    lines = game.get_lines
+    selected_space = false
+
+    lines.each do |line|
+      if line.uniq.size == 2
+
+        line_clone = line.clone.delete_if {|x| x.is_a? String}
+
+        unless line_clone.empty?
+
+          if line.include?(@which_player)
+            selected_space = line_clone.first
+          else
+            selected_space ||= line_clone.first
+          end
+        end
+      end
+    end
+
+    return selected_space
+  end
+
 
 end
--- computer_player_test.rb
+++ computer_player_test.rb
@@ -37,7 +37,36 @@ class ComputerPlayerTest < Test::Unit::TestCase
   end
 
   def test_cpus_complete_for_win_or_block_method_accepts_an_argument
-    assert_nothing_raised(ArgumentError) {@cpu.complete_for_win_or_block(@game.board)}
+    assert_nothing_raised(ArgumentError) {@cpu.complete_for_win_or_block(@game)}
+  end
+
+  def test_complete_for_win_or_block_returns_false_if_it_cant_block_or_complete_for_win
+    assert_equal false, @cpu.complete_for_win_or_block(@game)
+  end
+
+  def test_cfw_or_block_returns_number_of_space_to_block
+    @game.board[0] = 'O'
+    @game.board[1] = 'O'
+    assert_equal 3, @cpu.complete_for_win_or_block(@game)
+  end
+
+  def test_cfw_or_block_returns_number_of_space_to_complete_for_win
+    @game.board[0] = 'X'
+    @game.board[1] = 'X'
+    assert_equal 3, @cpu.complete_for_win_or_block(@game)
+  end
+
+  def test_cfw_or_block_completes_for_win_rather_than_blocking_when_both_possible
+    @game.board[0] = 'O'
+    @game.board[1] = 'O'
+    @game.board[6] = 'X'
+    @game.board[7] = 'X'
+    assert_equal 9, @cpu.complete_for_win_or_block(@game)
+  end
+
+  def test_cfw_or_block_returns_false_when_line_is_fully_marked
+    @game.board = [ 'X', 'O', 'O', 4, 5, 6, 7, 8, 9 ]
+    assert_equal false, @cpu.complete_for_win_or_block(@game)
   end
 
 
