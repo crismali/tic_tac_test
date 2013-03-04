037b92d91116de98bc8cbf5204914c8eaa0db574
Michael Crismali <michael.crismali@gmail.com>
2013-03-04 11:44:32 -0600

    rewrote game over logic

--- setup.rb
+++ setup.rb
@@ -45,35 +45,42 @@ module Setup
 
   end
 
-  def game_over
+  def game_over(opponent_class, who_goes_first)
     system ('clear')
     draw_board
 
-    puts "#{@who_won}'s win!" unless @who_won == "Cat's game!"
+    puts "#{@who_won}'s win!" unless @who_won == true
 
-    if @who_won == "Cat's game!"
-      puts @who_won
-    elsif @who_won == 'X' && @player_2.class == HumanPlayer
-      puts "Good job, player 1!"
-      puts "Better luck next time player 2"
-    elsif @who_won == 'O' && @player_2.class == HumanPlayer
-      puts "Tough luck, player 1."
-      puts "Good job, player 2" if @player_2.class == HumanPlayer
-    elsif @players_array.first.class == HumanPlayer && @who_won == 'X'
-      puts "Good job, whoever you are!"
-      puts "But beware... every time Skynet loses, it becomes smarter..."
-    elsif @players_array.first.class == HumanPlayer && @who_won == 'O'
-      puts "Tough luck, whoever you are"
-      puts "Especially because now that Skynet has mastered Tic-Tac-Toe, "
-      puts "it will focus on global conquest!"
-    elsif @players_array.first.class == ComputerPlayer && @who_won == 'X'
-      puts "Tough luck, whoever you are"
-      puts "Especially because now that Skynet has mastered Tic-Tac-Toe, "
-      puts "it will focus on global conquest!"
-    elsif @players_array.first.class == ComputerPlayer && @who_won == 'O'
-      puts "Good job, whoever you are!"
-      puts "But beware... every time Skynet loses, it becomes smarter..."
+    if opponent_class == HumanPlayer
+      puts 'Good job, player 1' if @who_won == 'X'
+      puts 'Good job, player 2' if @who_won == 'O'
+      puts 'Better luck next time, player 1' if @who_won == 'O'
+      puts 'Better luck next time, player 2' if @who_won == 'X'
+    elsif opponent_class == ComputerPlayer
+      if @who_won == 'X'
+        if who_goes_first == ComputerPlayer
+          puts "Tough luck, whoever you are"
+          puts "Especially because now that Skynet has mastered Tic-Tac-Toe, "
+          puts "it will focus on global conquest!"
+        else
+          puts "Good job, whoever you are!"
+          puts "But beware... every time Skynet loses, it becomes smarter..."
+        end
+      elsif @who_won == 'O'
+        if who_goes_first == HumanPlayer
+          puts "Tough luck, whoever you are"
+          puts "Especially because now that Skynet has mastered Tic-Tac-Toe, "
+          puts "it will focus on global conquest!"
+        else
+          puts "Good job, whoever you are!"
+          puts "But beware... every time Skynet loses, it becomes smarter..."
+        end
+
+      end
+    else
+      puts 'It was a draw'
     end
+
   end
 
   def want_to_play_again?
