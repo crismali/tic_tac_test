a7a013f682e8e54fdec83125fc38889a23c4160e
Michael Crismali <michael.crismali@gmail.com>
2013-03-04 12:31:07 -0600

    added draw board method

--- game.rb
+++ game.rb
@@ -11,5 +11,19 @@ class Game
     @who_won = false
   end
 
+  def draw_board
+   system ('clear')
+
+   puts "     |     |      "
+   puts "  #{ @board[0] }  |  #{ @board[1] }  |  #{ @board[2] }"
+   puts "_____|_____|_____"
+   puts "     |     |    "
+   puts "  #{ @board[3] }  |  #{ @board[4] }  |  #{ @board[5] }"
+   puts "_____|_____|_____"
+   puts "     |     |    "
+   puts "  #{ @board[6] }  |  #{ @board[7] }  |  #{ @board[8] }"
+   puts "     |     |    "
+ end
+
 
 end
