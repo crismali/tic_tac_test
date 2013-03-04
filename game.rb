67ecf8ffceb70a51595ceb5b80bd39db162dbba4
Michael Crismali <michael.crismali@gmail.com>
2013-03-04 11:27:07 -0600

    gave games a who won attribute and getter/setter methods

--- game.rb
+++ game.rb
@@ -3,11 +3,11 @@ class Game
   include GetLines
   include VictoryChecks
 
-  attr_accessor :board
+  attr_accessor :board, :who_won
 
   def initialize
     @board = [1,2,3,4,5,6,7,8,9]
-
+    @who_won = false
   end
 
 
