a7ec5b4685a13395ff7211b0a8f97d9cc525a9e9
Michael Crismali <michael.crismali@gmail.com>
2013-03-04 20:53:39 -0600

    refactored victory checks

--- victory_checks.rb
+++ victory_checks.rb
@@ -1,16 +1,16 @@
 module VictoryChecks
 
   def draw?
-    @who_won = @board.uniq.size == 2
+    @who_won = @board.uniq.size == 2 unless @who_won
+    @who_won
   end
 
   def anybody_win?
     lines = get_lines
-    someone_won = false
     lines.each do |line|
-      someone_won = line.first if line.uniq.size == 1
+      @who_won = line.first if line.uniq.size == 1
     end
-    @who_won = someone_won
+    @who_won
   end
 
 
