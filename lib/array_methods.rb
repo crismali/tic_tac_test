43b459537690c7a6d586119b374250d9a00b5b62
Michael Crismali <michael.crismali@gmail.com>
2013-03-25 13:33:28 -0500

    fixed indentation

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,6 @@
 class Array
 
-def include_any?(array)
+  def include_any?(array)
     new_array = Array.new
     array.each do |array_part|
       new_array << array_part if self.include? array_part
