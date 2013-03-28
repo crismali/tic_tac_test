331b2c7f1a342c4880e39ad7717c300e0ff767ec
Michael Crismali <michael.crismali@gmail.com>
2013-03-28 14:26:14 -0500

    refactored mark the board method, made changing the board its own method

--- lib/human_player.rb
+++ lib/human_player.rb
@@ -10,12 +10,13 @@ class HumanPlayer
     puts "Please enter a number that corresponds to an unmarked space."
     puts "(psst, in case you forgot, you're #{@which_player}'s)"
     input = false
-    input = test.first unless test.empty?
+    input = test.first.to_i unless test.empty?
     while true
-      input = get_human_input unless test.first.to_i > 0 && test.first.to_i < 10
-      break if valid_human_input?(input.to_i, board)
+      input = get_human_input.to_i unless test.first.to_i > 0 && test.first.to_i < 10
+      puts "input = #{input}"
+      break if valid_human_input?(input, board)
     end
-    board[input.to_i - 1] = @which_player unless space_marked? board[input.to_i - 1]
+    change_the_board(board, input - 1)
   end
 
   def get_human_input(*number_string)
@@ -31,6 +32,10 @@ class HumanPlayer
       puts "that correspond to an unmarked space are accepted"
       return false
     end
+  end
+
+  def change_the_board(board, input)
+    board[input] = @which_player unless space_marked? board[input]
   end
 
   def space_marked?(space)
