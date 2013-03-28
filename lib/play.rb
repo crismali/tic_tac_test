bf7ceafb62da0b6e6a79b9e6e8bd99faee3af0c4
Michael Crismali <michael.crismali@gmail.com>
2013-03-28 10:54:59 -0500

    finally refactored play

--- lib/play.rb
+++ lib/play.rb
@@ -1,41 +1,14 @@
 require './setup.rb'
+require './board.rb'
 require './game.rb'
 require './human_player.rb'
 require './computer_player.rb'
 
 while true
-
   game = Game.new
-
   game.introduce_game
-
-  opponent_class = game.play_against_friend_or_cpu?
-
-  who_goes_first = false
-
-  who_goes_first = game.who_goes_first? if opponent_class == ComputerPlayer
-
-
-  if who_goes_first == opponent_class
-    player_1 = opponent_class.new('X')
-    player_2 = HumanPlayer.new('O')
-  else
-    player_1 = HumanPlayer.new('X')
-    player_2 = opponent_class.new('O')
-  end
-
-  players = player_1, player_2
-
-  9.times do |i|
-    game.draw_board if players[i % 2].is_a? HumanPlayer
-    players[i % 2].mark_the_board(game.board)
-    game.anybody_win?
-    game.draw?
-    break if game.who_won
-  end
-
-  game.game_over(opponent_class, who_goes_first)
-
+  player_classes = game.play_against_friend_or_cpu?
+  game.play( [ player_classes.first.new('X'), player_classes.last.new('O') ] )
+  game.game_over
   break unless game.want_to_play_again?
-
 end
