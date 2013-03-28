835748f873a7f22e3d429bb03a3f5430f9e2a7b8
Michael Crismali <michael.crismali@gmail.com>
2013-03-28 10:53:07 -0500

    finally refactored setup

--- lib/setup.rb
+++ lib/setup.rb
@@ -13,67 +13,54 @@ module Setup
   def play_against_friend_or_cpu?
     puts "\nWould you like to play against a friend (option 1)"
     puts "or against the computer (option 2)?\n"
-    player_2_class = false
-    until player_2_class
+    player_classes = false
+    until player_classes
       user_input = gets.chomp
       if user_input == "1"
-        player_2_class = HumanPlayer
+        player_classes = HumanPlayer, HumanPlayer
       elsif user_input == "2"
-        player_2_class = ComputerPlayer
+        player_classes = HumanPlayer, ComputerPlayer
       else
         puts "Please enter a 1 (for a human opponent) or a 2 (for a computer opponent)."
       end
     end
-    return player_2_class
+    player_classes = who_goes_first?(player_classes) if player_classes.include? ComputerPlayer
+    player_classes
   end
 
-  def who_goes_first?
+  def who_goes_first?(player_classes)
     puts "\nWould you like to go first?"
     while true
       user_input = gets.chomp
       if user_input[0] == 'y'
-        return HumanPlayer
+        return player_classes
       elsif user_input[0] == 'n'
-        return ComputerPlayer
+        return player_classes.reverse
       else
         puts "Please enter yes or no"
       end
     end
   end
 
-  def game_over(opponent_class, who_goes_first)
+  def game_over
     system ('clear')
     draw_board
-    puts "#{@who_won}'s win!" unless @who_won == true
-    if @who_won == true
-       puts 'It was a tie!'
-    elsif opponent_class == HumanPlayer
-      puts "Good job, #{@who_won}'s!"
-      puts "Better luck next time, #{@who_won == 'X' ? 'O' : 'X'}'s."
-    elsif opponent_class == ComputerPlayer
-      if @who_won == 'X'
-        if who_goes_first == ComputerPlayer
-          puts "Tough luck, whoever you are."
-          puts "...Especially because now that Skynet has mastered Tic-Tac-Toe, "
-          puts "it will focus on global conquest!"
-          puts "...Unless you'd like to play again that is..."
-        else
-          puts "Good job, whoever you are!"
-          puts "But beware... every time Skynet loses, it becomes smarter..."
-        end
-      elsif @who_won == 'O'
-        if who_goes_first == HumanPlayer
-          puts "Tough luck, whoever you are."
-          puts "Especially because now that Skynet has mastered Tic-Tac-Toe, "
-          puts "it will focus on global conquest!"
-          puts "...Unless you'd like to play again that is..."
-        else
-          puts "Good job, whoever you are!"
-          puts "But beware... every time Skynet loses, it becomes smarter..."
-        end
+    puts "#{who_won.which_player}'s win!" if who_won
+    puts 'It was a tie!' unless who_won
+    if players.map{|x| x.class}.include? ComputerPlayer
+      if who_won.is_a? ComputerPlayer
+        puts "Tough luck, whoever you are."
+        puts "...Especially because now that Skynet has mastered Tic-Tac-Toe, "
+        puts "it will focus on global conquest!"
+        puts "...Unless you'd like to play again that is..."
+      elsif who_won
+        puts "Good job, whoever you are!"
+        puts "But beware... every time Skynet loses, it becomes smarter..."
       end
+    elsif who_won
+      puts "Good job, #{who_won.which_player}'s!"
+      puts "Better luck next time, #{who_won.which_player == 'X' ? 'O' : 'X'}'s."
     end
-
   end
 
   def want_to_play_again?
